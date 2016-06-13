ebd6373697b8aabfef471a323b289892af0eae79
hans-sperling <info@bluejade.de>
2016-06-13 16:45:55 +0200

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # perspective-view
-
-Javascript library for rendering grid based maps into a virtual, perspective, 3d top view with canvas.
--- dev/js/app.js
+++ dev/js/app.js
@@ -58,22 +58,19 @@ jQuery(document).ready(function() {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ],
-        canvas       = document.getElementById('myCanvas'),
+        $canvas      = $('#PerspectiveView'),
+        canvas       = $canvas[0],
         context      = canvas.getContext('2d');
 
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 
 
-    var pv = new PerspectiveView();
-
-    console.log(pv);
-
-    pv.setConfig({
-        canvas:         document.getElementById('myCanvas'),
-        context:        document.getElementById('myCanvas').getContext('2d'),
-        depth:          0.05,
-        renderMode:     'specified',
+    var config = {
+        canvas:         $canvas,
+        context:        context,
+        //depth:          0.05,
+        //renderMode:     'specified',
         //renderMode:     'flat',
         //renderMode:     'unitary',
         map:            realisticMap,
@@ -81,9 +78,10 @@ jQuery(document).ready(function() {
         //map:            heightMap,
         unitSize:       {x: 40,  y: 40},
         vanishingPoint: {x: 260, y: 180}
-    });
+    };
 
-    pv.getModule('color').setTime(12);
+    var ppv = new PerspectiveView(config);
 
-    pv.render();
+    console.dir(ppv);
+
 });
