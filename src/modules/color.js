124196a947e468df1e8486fb95b455bcc341e7ef
hans-sperling <info@bluejade.de>
2016-07-21 08:25:51 +0200

    Change workplace

--- src/modules/color.js
+++ src/modules/color.js
@@ -8,16 +8,36 @@
 
     // ------------------------------------------------------------------------------------------------ MODULE INTERFACE
 
+    /**
+     * Initializes this module; will be called at the beginning from the app. Updates the module with the given config.
+     *
+     * @public
+     * @param {object} config
+     * @return {void}
+     */
     function init(config) {
         update(config);
     }
 
 
+    /**
+     * Will be called from app if all other modules has been loaded.
+     *
+     * @public
+     * @return {void}
+     */
     function run() {
         // Nothing to do yet
     }
 
 
+    /**
+     * Updates this module, will be called on init and on general updating the app.
+     *
+     * @public
+     * @pram {object} config
+     * @return {void}
+     */
     function update(config) {
         CFG = config;
 
@@ -26,7 +46,13 @@
 
     // --------------------------------------------------------------------------------------------------------- METHODS
 
-
+    /**
+     * Returns the rgba-color string of the base color computed with the given lightning percentage.
+     *
+     * @private
+     * @param {number} lighting - Percentage number to compute with the base color
+     * @returns {string} - 'rgba(r, g, b, a)'
+     */
     function getColor(lighting) {
         var color = CFG.color.objectColor;
 
@@ -38,11 +64,23 @@
     }
 
 
+    /**
+     * Returns the rgba-color string of the back shape of an object.
+     *
+     * @public
+     * @returns {string}
+     */
     function getBack() {
         return getColor(lighting.back);
     }
 
 
+    /**
+     * Returns the rgba-color string of the empty space shape .
+     *
+     * @public
+     * @returns {string}
+     */
     function getSpace() {
         var color = CFG.color.spaceColor;
 
@@ -54,26 +92,57 @@
     }
 
 
-    function getFront(h) {
-        return getColor(lighting.front + (h * lighting.height));
+    /**
+     * Returns the rgba-color string of the front shape of an object computed with a current given height.
+     *
+     * @public
+     * @param {number} height
+     * @returns {string}
+     */
+    function getFront(height) {
+        return getColor(lighting.front + (height * lighting.height));
     }
 
 
+    /**
+     * Returns the rgba-color string of the north shape of an object.
+     *
+     * @public
+     * @returns {string}
+     */
     function getNorth() {
         return getColor(lighting.north);
     }
 
 
+    /**
+     * Returns the rgba-color string of the east shape of an object.
+     *
+     * @public
+     * @returns {string}
+     */
     function getEast() {
         return getColor(lighting.east);
     }
 
 
+    /**
+     * Returns the rgba-color string of the south shape of an object.
+     *
+     * @public
+     * @returns {string}
+     */
     function getSouth() {
         return getColor(lighting.south);
     }
 
 
+    /**
+     * Returns the rgba-color string of the west shape of an object.
+     *
+     * @public
+     * @returns {string}
+     */
     function getWest() {
         return getColor(lighting.west);
     }
